cad74795ceca109c953a1ff436f5a9f4e5173353
Karmjit Singh <karmjit.singh@purple.telstra.com>
2021-10-13 08:29:12 +1000

    Launch screen demo app

---
+++ LaunchScreenSample/LaunchScreenSample/LaunchScreenSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchScreenSampleApp.swift
+//  LaunchScreenSample
+//
+//  Created by karmjit singh on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct LaunchScreenSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
